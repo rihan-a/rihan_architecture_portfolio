cfef9858c37dffde8d363a7d5804c0493639201f
rihan_a <a.rihan@live.com>
2023-01-27 17:29:04 +0200

    add FeaturedProject component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import Cover from "./components/Cover/Cover";
+import FeaturedProjects from "./components/FeaturedProjects/FeaturedProjects";
 
 function App() {
-    return <Cover />;
+    return (
+        <>
+            <Cover />
+            <FeaturedProjects />
+        </>
+    );
 }
 
 export default App;
