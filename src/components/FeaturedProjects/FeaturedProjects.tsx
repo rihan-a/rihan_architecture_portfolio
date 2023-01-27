95967fec39d563c831233042ffe19bb7c18e8dc3
rihan_a <a.rihan@live.com>
2023-01-27 17:10:12 +0200

    create FeaturedProjects component

---
+++ src/components/FeaturedProjects/FeaturedProjects.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function FeaturedProjects() {
+    return (
+        <div className="featured-projects-wrapper">
+            <div className="featured-projects-text">
+                <h2>SELECTED WORKS 2013 / 2022</h2>
+            </div>
+            <div className="featured-projects-imgs">
+                <img src="images/092_CAVALLINO_V3_P.jpg" alt="" />
+            </div>
+        </div>
+    );
+}
+
+export default FeaturedProjects;
