667d1a89bdbbe815ee938cc81b036051de7940f4
rihan_a <a.rihan@live.com>
2023-01-27 17:29:54 +0200

    update FeaturedProjects jsx

--- src/components/FeaturedProjects/FeaturedProjects.tsx
+++ src/components/FeaturedProjects/FeaturedProjects.tsx
@@ -1,15 +1,16 @@
 import React from "react";
+import "./FeaturedProjects.css";
 
 function FeaturedProjects() {
     return (
-        <div className="featured-projects-wrapper">
+        <section className="featured-projects-container main-container">
             <div className="featured-projects-text">
-                <h2>SELECTED WORKS 2013 / 2022</h2>
+                <h3>SELECTED WORKS 2013 / 2022</h3>
             </div>
             <div className="featured-projects-imgs">
                 <img src="images/092_CAVALLINO_V3_P.jpg" alt="" />
             </div>
-        </div>
+        </section>
     );
 }
 
