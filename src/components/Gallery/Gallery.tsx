ac87c2a2fa337289b6b2cfd6ef8db0c4504bdc7a
rihan_a <a.rihan@live.com>
2023-01-28 16:28:52 +0200

    update gallery to return projects from json

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import "./Gallery.css";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
+import projects from "../../projects.json";
+
+import { projectInterface } from "../../projectInterface";
+
 function Gallery() {
     return (
         <div className="gallery-container main-container">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+            {projects.map((project: projectInterface) => {
+                return (
+                    <ProjectCard
+                        key={project.id}
+                        coverImg={project.images[0]}
+                        title={project.title}
+                    />
+                );
+            })}
         </div>
     );
 }
