becb85776578303822a1574e06fd281451913820
rihan_a <a.rihan@live.com>
2023-01-28 16:27:18 +0200

    create an interface for project object

---
+++ src/projectInterface.ts
@@ -0,0 +1,11 @@
+export interface projectInterface {
+    id: number;
+    title: string;
+    desc: string;
+    year: number;
+    location: string;
+    client: string;
+    type: string;
+    role: string;
+    images: string[];
+}
