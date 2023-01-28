9d81d562b33134fd4ed93e1be76053eb6ae570dd
rihan_a <a.rihan@live.com>
2023-01-28 15:31:45 +0200

    insert ProjectCards components to test

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./Gallery.css";
+import ProjectCard from "../ProjectCard/ProjectCard";
 
 function Gallery() {
-    return <div>Gallery</div>;
+    return (
+        <div>
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+            <ProjectCard />
+        </div>
+    );
 }
 
 export default Gallery;
