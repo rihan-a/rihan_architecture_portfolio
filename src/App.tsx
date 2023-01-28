1bb23bc006db696ade2541a45294c79470f6ed8a
rihan_a <a.rihan@live.com>
2023-01-28 15:50:00 +0200

    import Gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Cover from "./components/Cover/Cover";
 import FeaturedProjects from "./components/FeaturedProjects/FeaturedProjects";
+import Gallery from "./components/Gallery/Gallery";
 
 function App() {
     return (
         <>
             <Cover />
             <FeaturedProjects />
+            <Gallery />
         </>
     );
 }
