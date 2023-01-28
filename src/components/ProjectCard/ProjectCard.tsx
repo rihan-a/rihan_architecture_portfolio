f842ea05ed8e157e8776ec55a5f9713044d7b917
rihan_a <a.rihan@live.com>
2023-01-28 15:31:22 +0200

    add project img and title jsx structure

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import "./ProjectCard.css";
 
 function ProjectCard() {
-    return <div>ProjectCard</div>;
+    return (
+        <div className="project-card-container">
+            <div className="project-card-img">
+                <img src="/images/ATHORA_MUNICH_V1.jpg" alt="" />
+            </div>
+            <div className="project-title">
+                <h4>Project Name</h4>
+            </div>
+        </div>
+    );
 }
 
 export default ProjectCard;
