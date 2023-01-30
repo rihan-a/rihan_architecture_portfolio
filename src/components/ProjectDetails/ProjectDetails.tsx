c58ed26f89ac6116ae92ac67aa48478b95cad1b3
rihan_a <a.rihan@live.com>
2023-01-30 20:04:50 +0200

    create ProjectDetails component

---
+++ src/components/ProjectDetails/ProjectDetails.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ProjectDetails.css";
+
+function ProjectDetails() {
+    return (
+        <section className="project-wrapper main-container">
+            <div className="project-text-wrapper">
+                <div className="project-name">
+                    <h3>Project Name</h3>
+                </div>
+
+                <div className="project-desc">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Saepe dolore incidunt, sed sequi placeat eaque unde! Sit
+                        fugiat vitae rem nemo praesentium iure quas quasi eum, a
+                        saepe, adipisci voluptatibus. Lorem ipsum, dolor sit
+                        amet consectetur adipisicing elit. Nam quod libero
+                        quaerat quibusdam perferendis accusamus blanditiis
+                        quisquam sint autem. Officia necessitatibus dignissimos
+                        amet numquam repudiandae error sapiente magni odio sint.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default ProjectDetails;
