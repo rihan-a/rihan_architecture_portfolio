d16493db713c1d85cf4abf0b6482ff2eaf4dbda6
rihan_a <a.rihan@live.com>
2023-01-30 19:47:29 +0200

    create Home component

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Cover from "../Cover/Cover";
+import FeaturedProjects from "../FeaturedProjects/FeaturedProjects";
+import Gallery from "../Gallery/Gallery";
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+            <Cover />
+            <FeaturedProjects />
+            <Gallery />
+        </>
+    );
+}
+
+export default Home;
