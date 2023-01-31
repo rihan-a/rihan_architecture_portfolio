16c0905bd77f28c748a19c2a4e1ce0ed18cb1b8a
rihan_a <a.rihan@live.com>
2023-01-31 17:53:52 +0200

    get project data using id

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProjectDetails from "../ProjectDetails/ProjectDetails";
+import { useSelector } from "react-redux";
+
+import projects from "../../projects.json";
+
+interface projectType {
+    id: number | null;
+    title: string;
+    desc: string;
+    year: number | null;
+    location: string;
+    client: string;
+    type: string;
+    role: string;
+    images: string[];
+}
 
 function Project() {
-    return <ProjectDetails />;
+    const projectId: number = useSelector((state: any) => {
+        return state.project.projectId;
+    });
+
+    const [projectData, setProjectData] = useState<projectType>({
+        id: null,
+        title: "",
+        desc: "",
+        year: null,
+        location: "",
+        client: "",
+        type: "",
+        role: "",
+        images: [],
+    });
+
+    useEffect(() => {
+        const projectData = projects.filter((project: projectType) => {
+            return projectId === project.id;
+        });
+        setProjectData(projectData[0]);
+    }, [projectId]);
+
+    return <ProjectDetails projectData={projectData} />;
 }
 
 export default Project;
