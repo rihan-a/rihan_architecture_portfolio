23a0f4b1669cc3548b80964f035d485ca64370e4
rihan_a <a.rihan@live.com>
2023-01-31 21:02:11 +0200

    add nav links and logo

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./NavBar.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
-    return <div className="nav-bar"></div>;
+    return (
+        <div className="nav-bar">
+            <Link className="nav-bar-link" to="/">
+                HOME
+            </Link>
+            <img src="/images/LOGO.png" alt="rihan-logo" />
+            <Link className="nav-bar-link" to="/">
+                CONTACT
+            </Link>
+        </div>
+    );
 }
 
 export default NavBar;
