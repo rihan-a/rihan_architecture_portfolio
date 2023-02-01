b5e62f333bd54f8b9db99c4eb2c030afcac23aa7
rihan_a <a.rihan@live.com>
2023-02-01 23:33:42 +0200

    change featured projects text

--- src/components/FeaturedProjects/FeaturedProjects.tsx
+++ src/components/FeaturedProjects/FeaturedProjects.tsx
@@ -5,7 +5,7 @@ function FeaturedProjects() {
     return (
         <section className="featured-projects-container main-container">
             <div className="featured-projects-text">
-                <h3>SELECTED WORKS 2013 / 2022</h3>
+                <h3>SELECTED WORKS 2010 / 2022</h3>
             </div>
             <div className="featured-projects-imgs">
                 <img src="images/092_CAVALLINO_V3_P.jpg" alt="" />
