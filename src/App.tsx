a0ed9916b5748cf242bd8c21f31a73e2e8f16fe5
rihan_a <a.rihan@live.com>
2023-02-01 14:30:23 +0200

    import contact component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Home from "./components/Home/Home";
 import Project from "./components/Project/Project";
+import Contact from "./components/Contact/Contact";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/project/:id" element={<Project />}></Route>
+                    <Route path="/contact" element={<Contact />}></Route>
                 </Routes>
             </BrowserRouter>
         </>
