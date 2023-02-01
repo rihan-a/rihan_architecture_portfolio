75215c5ca1712329fa7e637c35a2d6640b0ca2a9
rihan_a <a.rihan@live.com>
2023-02-01 14:34:32 +0200

    add logic for generating random cover image on load

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MouseIcon from "../MouseIcon/MouseIcon";
 import "./Cover.css";
+import projects from "../../projects.json";
 
 function Cover() {
+    const [coverUrl, setCoverUrl] = useState<string>("");
+
+    useEffect(() => {
+        let projectRandomNumber = Math.floor(Math.random() * 9);
+        let imageRandomNumber = Math.floor(Math.random() * 3);
+
+        const coverImageUrl =
+            projects[projectRandomNumber].images[imageRandomNumber];
+        setCoverUrl(coverImageUrl);
+    }, []);
+
     return (
         <div className="cover-wrapper">
-            <div className="cover">
+            <div
+                className="cover"
+                style={{
+                    backgroundImage: `url("${coverUrl}")`,
+                }}
+            >
                 <h1>PORTFOLIO</h1>
             </div>
             <div className="mouse-icon-row">
