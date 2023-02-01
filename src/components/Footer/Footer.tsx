ee2353022877709bdd577f32956b416339998a68
rihan_a <a.rihan@live.com>
2023-02-01 16:40:44 +0200

    create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <div className="footer">
+                <p>
+                    Rihan Architecture Portfolio © 2023 All rights reserved. -
+                    Designed and Developed by Ahmed Rihan
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
