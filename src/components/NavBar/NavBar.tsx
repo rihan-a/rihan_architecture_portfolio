53d7930489c80f3ce561055874178c72897ae386
rihan_a <a.rihan@live.com>
2023-02-03 19:47:01 +0200

    add onclick on logo to navigate to home

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import "./NavBar.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 
 function NavBar() {
+    const navigate = useNavigate();
     return (
         <div className="nav-bar">
             <Link className="nav-bar-link" to="/">
                 HOME
             </Link>
             <div className="navbar-logo">
-                <img src="/images/LOGO.png" alt="rihan-logo" />
+                <img
+                    src="/images/LOGO.png"
+                    alt="rihan-logo"
+                    onClick={() => navigate("/")}
+                />
             </div>
             <Link className="nav-bar-link" to="/contact">
                 CONTACT
