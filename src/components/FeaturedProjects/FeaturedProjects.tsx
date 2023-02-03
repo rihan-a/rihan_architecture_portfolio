af5f45ff161d20badfd4e111c756b7f1918395a3
rihan_a <a.rihan@live.com>
2023-02-03 18:37:21 +0200

    generate random featured project on load

--- src/components/FeaturedProjects/FeaturedProjects.tsx
+++ src/components/FeaturedProjects/FeaturedProjects.tsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./FeaturedProjects.css";
 import coverImages from "../../coverImages.json";
 
 function FeaturedProjects() {
+    const [projectUrl, setProjectUrl] = useState<string>("");
+
+    useEffect(() => {
+        let imageRandomNumber = Math.floor(Math.random() * 17);
+        const projectImageUrl = coverImages[0].coverImages[imageRandomNumber];
+
+        setProjectUrl(projectImageUrl);
+    }, []);
+
     return (
         <section className="featured-projects-container">
             <div className="featured-projects-text">
                 <h3>SELECTED WORKS 2010 / 2022</h3>
             </div>
             <div className="featured-projects-imgs">
-                <img src="images/092_CAVALLINO_V3_P.jpg" alt="" />
+                <img src={projectUrl} alt="" />
             </div>
         </section>
     );
