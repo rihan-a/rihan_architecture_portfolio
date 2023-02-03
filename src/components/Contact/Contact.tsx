a8028b654901d418ad3b0b18edeca547ddcc0456
rihan_a <a.rihan@live.com>
2023-02-03 16:44:12 +0200

    import contactForm

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Contact.css";
 import NavBar from "../NavBar/NavBar";
+import ContactForm from "../ContactForm/ContactForm";
 
 function Contact() {
     return (
         <div>
             <NavBar />
+            <ContactForm />
         </div>
     );
 }
