fcfa11ca576b5c5fdcb570bbb22ba8667e21542c
rihan_a <a.rihan@live.com>
2023-02-04 18:31:33 +0200

    add logic to filter project by types

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Gallery.css";
 import ProjectCard from "../ProjectCard/ProjectCard";
 
@@ -12,16 +12,40 @@ interface projectCard {
 }
 
 function Gallery() {
+    const [projectFilter, setProjectFilter] = useState<string>("All");
+
+    const filterProjects = (e: React.MouseEvent<HTMLElement>) => {
+        setProjectFilter(e.currentTarget.innerHTML);
+    };
+
     return (
         <>
             <div className="projects-programme">
                 {projectTypes.map((type: string) => {
-                    return <h4 className="programme-txt">{type}</h4>;
+                    return (
+                        <h4
+                            className={
+                                projectFilter === type
+                                    ? "programme-txt active"
+                                    : "programme-txt"
+                            }
+                            key={type}
+                            onClick={filterProjects}
+                        >
+                            {type}
+                        </h4>
+                    );
                 })}
             </div>
             <div className="gallery-container main-container">
                 {projects
                     .sort((a, b) => 0.5 - Math.random())
+                    .filter((project) => {
+                        if (projectFilter === "All") {
+                            return project.programme;
+                        }
+                        return project.programme === projectFilter;
+                    })
                     .map((project: projectCard) => {
                         return (
                             <ProjectCard
