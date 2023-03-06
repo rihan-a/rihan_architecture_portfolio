a3225fcbedfb127fa4cb0dcf11c28691b09f789c
rihan_a <a.rihan@live.com>
2023-03-06 21:39:37 +0100

    change div tag to footer tag

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,14 +3,14 @@ import "./Footer.css";
 
 function Footer() {
     return (
-        <div className="footer-container">
+        <footer className="footer-container">
             <div className="footer">
                 <p>
                     Architecture Portfolio © 2023 All rights reserved - Designed
                     and Developed by Ahmed Rihan
                 </p>
             </div>
-        </div>
+        </footer>
     );
 }
 
