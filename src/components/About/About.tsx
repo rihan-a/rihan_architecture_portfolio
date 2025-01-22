27cb098735ed0b75c2f4b9c5bfc04d63b75ea73b
rihan-a <a.rihan@live.com>
2025-01-22 14:12:28 +0100

    update bio text

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -19,38 +19,44 @@ function About() {
 
                 <section className="about-txt">
                     <p>
-                        I’m an architect, designer, and technologist with
-                        multidisciplinary expertise spanning computational
-                        design, digital fabrication, and advanced project
-                        management. With over a decade in the industry, I’ve
-                        applied cutting-edge tools and techniques to
-                        architectural projects, including the preliminary design
-                        of Amsterdam’s 3D-printed bridge—a project realized
-                        through complex optimization algorithms. My work also
-                        includes high-profile initiatives like the Neom master
-                        plan and the German Expo pavilion in Dubai, where I
-                        brought innovative thinking to large-scale urban and
-                        exhibition spaces.
+                        I’m a software engineer, designer, and technologist with
+                        a unique background in architecture and advanced project
+                        management. With over a decade of experience in
+                        architecture and computational design, I’ve transitioned
+                        into software engineering to explore the transformative
+                        potential of technology and AI.
                     </p>
                     <p>
-                        In addition to my design expertise, I honed my project
-                        management skills while leading engineering teams at
-                        Tesla's Berlin Gigafactory. There, I played an
-                        instrumental role in driving complex projects to
-                        completion, effectively balancing technical challenges
-                        with tight timelines. This role required not only
-                        technical precision but also a strong aptitude for
-                        problem-solving and adaptability—skills I bring to every
-                        project.
+                        My career began with groundbreaking projects like the
+                        preliminary design of Amsterdam’s 3D-printed bridge,
+                        leveraging optimization algorithms, and large-scale
+                        initiatives like the Neom master plan and the German
+                        Expo pavilion in Dubai. Leading engineering teams at
+                        Tesla's Berlin Gigafactory further refined my skills in
+                        delivering complex, multidisciplinary projects under
+                        tight deadlines, sharpening my ability to balance
+                        technical challenges with innovative problem-solving.
+                    </p>
+
+                    <p>
+                        Over the past two years, I’ve immersed myself in
+                        software engineering, specializing in web development,
+                        AI integration, and building scalable, future-oriented
+                        solutions. I’m passionate about creating tools and
+                        platforms that bridge design, technology, and
+                        human-centric innovation. My expertise spans
+                        computational design, digital fabrication, project
+                        management, and now full-stack development, allowing me
+                        to approach challenges with a multidisciplinary
+                        perspective.
                     </p>
                     <p>
-                        Now based in Berlin, I’m exploring new realms in Project
-                        Managment, AI, driven by a passion for creating
-                        impactful, future-oriented solutions. My career reflects
-                        a continuous commitment to bridging architecture,
-                        technology, and entrepreneurial innovation, and I’m
-                        constantly pushing the boundaries of what design can
-                        achieve.
+                        Based in Berlin, I’m driven by a commitment to
+                        leveraging technology to create impactful solutions,
+                        from smart applications to AI-powered tools. My journey
+                        reflects a dedication to lifelong learning,
+                        adaptability, and pushing boundaries—qualities I bring
+                        to every project and team I join.
                     </p>
                 </section>
             </section>
