953a337d92e6d82307f873d198a55e39c3e8acc8
rihan-a <a.rihan@live.com>
2025-02-26 22:23:36 +0200

    Remove unused items + Update cover image url

--- src/components/Cover/Cover.tsx
+++ src/components/Cover/Cover.tsx
@@ -1,27 +1,13 @@
-import React, { useEffect, useState } from "react";
-import NavBar from "../NavBar/NavBar";
 import "./Cover.css";
-import coverImages from "../../coverImages.json";
-import { Link } from "react-router-dom";
 
 function Cover() {
-    // const [coverUrl, setCoverUrl] = useState<string>("");
-
-    // useEffect(() => {
-    //     let imageRandomNumber = Math.floor(Math.random() * 11);
-    //     const coverImageUrl = coverImages[0].coverImages[imageRandomNumber];
-
-    //     setCoverUrl(coverImageUrl);
-    // }, []);
-
     return (
         <>
-            <NavBar />
             <main className="cover-wrapper">
                 <a href="/genai">
                     <img
                         className="genspace-cover"
-                        src="https://rihanbucket.s3.us-east-1.amazonaws.com/genspace-ai/GenSpace+Ai.webp"
+                        src="https://rihanbucket.s3.us-east-1.amazonaws.com/genspace-ai/GenSpace+Ai-cover.webp"
                         alt="genspace ai cover"
                     />
                 </a>
